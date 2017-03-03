0bf639e6006f66bb69fad79e95f4ca2464bd946d
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-03-02 16:55:11 -0800

    splitting design matrix

--- notest.R
+++ notest.R
@@ -38,9 +38,18 @@ X <- model.matrix(Y ~ . , data=co2)
 
 mf <- model.frame(Y ~ . , data=co2)
 a <- splitFrame(mf, type='f') # type = c("f","fi", "fii"))
+head(a$x1) # x1 = factors, x2 = continuous
+head(a$x2)
 
-Z <- X[, 6:8]
-X <- X[, 1:5]
-old.SMPY(X=X, y=coleman$Y, Z=Z, intercept=TRUE)
+mf <- model.frame(Y ~ .*educ , data=co2)
+a <- splitFrame(mf, type='fi') # type = c("f","fi", "fii"))
+head(a$x1) # x1 = factors + interactions
+head(a$x2) # x2 = continuous
+
+mf <- model.frame(Y ~ . -1, data=co2)
+a <- splitFrame(mf, type='f') # type = c("f","fi", "fii"))
+Z <- a$x1
+X <- a$x2
+old.SMPY(X=X, y=coleman$Y, Z=Z, intercept=FALSE)
 
 
