6a29a6086230b5c59cb0c7d57e9efe44075dc0dc
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-03-23 10:42:23 -0700

    more dcml

--- R/lmrob2.R
+++ R/lmrob2.R
@@ -220,24 +220,19 @@ lmrob2 <-
         n <- length(z$residuals)
         dee <- control$bb
         if(control$corr.b) dee <- dee*(1-p/n)
-        si.dcml <- mscale(z$residuals, tol = control$mscale.tol, delta=dee, tuning.chi=control$tuning.chi)
+        si.dcml <- mscale(u=z$residuals, tol = control$mscale.tol, delta=dee, tuning.chi=control$tuning.chi)
         deltas <- .3*p/n 
         CC <- t(x * z$rweights) %*% x / sum(z$rweights) 	
+        # print(all.equal(CC, t(x) %*% diag(z$rweights) %*% x / sum(z$rweights)))
         d <- as.numeric(crossprod(beta.R-beta.LS, CC %*% (beta.R-beta.LS)))/si.dcml^2
         t0 <- min(1, sqrt(deltas/d))
         beta.dcml <- t0*coef(z0) +(1-t0)*coef(z)
-        V <- cov.dcml(res.LS=z0$residuals, res.R=z$residuals, CC=CC, 
+        V.dcml <- cov.dcml(res.LS=z0$residuals, res.R=z$residuals, CC=CC, 
                       sig.R=si.dcml, t0=t0, p=p, n=n, control=control) / n
-        # print(V / as.matrix(z$cov))
-        zz <- z$cov 
-        attributes(zz) <- NULL
-        print(matrix(zz, p, p)) # * z$scale)
-        print(V)
-        # V <- covdcml (z0$residuals,z$residuals, CC, si.dcml, t0, p, n) / n	
-        # resid=y-XX%*%beta1
-        # sigma=mscale(resid, .00001,dee) 
-        # out=list(coef=beta1, cov=V, resid=resid,   sigma=sigma )
+        re.dcml <- as.vector(y - x %*% beta.dcml)
+        si.dcml.final <- mscale(u=re.dcml, tol = control$mscale.tol, delta=dee, tuning.chi=control$tuning.chi)
         
+        # out=list(coef=beta.dcml, cov=V.dcml, resid=re.dclml,   sigma=si.dcml.final )
         
         
         # print('About to compute M step')
