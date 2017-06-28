d8555a1f0defb2faaf6ad5b018dadf0716d8fcf7
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-06-28 07:58:02 -0700

    large n/p is better

--- mm-ridge-test.R
+++ mm-ridge-test.R
@@ -11,8 +11,8 @@ library(mmlasso)
 
 library(glmnet)
 
-n <- 50
-p <- 20
+n <- 500
+p <- 10
 set.seed(123)
 x <- matrix(rnorm(n*p), n, p)
 y <- as.vector( x %*% c(rep(2, 5), rep(0, p-5))) + rnorm(n, sd=.5)
@@ -20,10 +20,10 @@ a <- sridge(x=x, y=y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0,
             denormout=0, alone=1, ncores=1)
 
 b <- pense(X=x, y=y, alpha=0, lambda=0, nlambda=1, standardize=TRUE, 
-           control=pense.control(mscale.delta = 0.3))
+           control=pense.control(mscale.delta = 0.49))
 
 b <- pense(X=x, y=y, alpha=0, lambda=seq(0, 5, length=20), standardize=TRUE, 
-           control=pense.control(mscale.delta = 0.3))
+           control=pense.control(mscale.delta = 0.49))
 
 
 d <- cv.glmnet(x=x, y=y, lambda=seq(0, 5, length=20), family='gaussian', 
