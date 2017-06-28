ce6396648d2471a77723d8bd9e65160c410c347d
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-06-28 08:58:47 -0700

    the lambda-grid is mysterious

--- mm-ridge-test.R
+++ mm-ridge-test.R
@@ -19,10 +19,10 @@ y <- as.vector( x %*% c(rep(2, 5), rep(0, p-5))) + rnorm(n, sd=.5)
 a <- sridge(x=x, y=y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0, 
             denormout=0, alone=1, ncores=1)
 
-b <- pense(X=x, y=y, alpha=0, lambda=0, nlambda=1, standardize=TRUE, 
+b0 <- pense(X=x, y=y, alpha=0, standardize=TRUE, 
            control=pense.control(mscale.delta = 0.49))
 
-b <- pense(X=x, y=y, alpha=0, lambda=seq(0, 5, length=20), standardize=TRUE, 
+b1 <- pense(X=x, y=y, alpha=0, lambda=seq(0, 5, length=20), standardize=TRUE, 
            control=pense.control(mscale.delta = 0.49))
 
 
