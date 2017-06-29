4ac33e6e48073b30adb0a635743e4170d61b60d9
Matias Salibian Barrera <matias@stat.ubc.ca>
2017-06-29 14:19:28 -0700

    use "develop" version of pense

--- mm-ridge-test.R
+++ mm-ridge-test.R
@@ -1,6 +1,7 @@
 
 # Check MM-ridge regression
 
+# install_github('gcohenfr/PENSE-package', ref='develop', auth_token='d49f60185d9de80d37dacef7f93b14df907e6af7')
 library(pense)
 
 # compare S-ridge as computed with pense
@@ -16,16 +17,16 @@ p <- 10
 set.seed(123)
 x <- matrix(rnorm(n*p), n, p)
 y <- as.vector( x %*% c(rep(2, 5), rep(0, p-5))) + rnorm(n, sd=.5)
-a <- sridge(x=x, y=y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0, 
+a <- sridge(x=x, y=y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0,
             denormout=0, alone=1, ncores=1)
 
-b0 <- pense(X=x, y=y, alpha=0, standardize=TRUE, 
+b0 <- pense(X=x, y=y, alpha=0, standardize=TRUE, lambda=seq(1e-7, 5, length=20)) #,
+#           control=pense.control(mscale.delta = 0.49))
+
+b1 <- pense(X=x, y=y, alpha=0, lambda=seq(0, 5, length=20), standardize=TRUE,
            control=pense.control(mscale.delta = 0.49))
 
-b1 <- pense(X=x, y=y, alpha=0, lambda=seq(0, 5, length=20), standardize=TRUE, 
-           control=pense.control(mscale.delta = 0.49))
 
-
-d <- cv.glmnet(x=x, y=y, lambda=seq(0, 5, length=20), family='gaussian', 
+d <- cv.glmnet(x=x, y=y, lambda=seq(0, 5, length=20), family='gaussian',
                intercept=TRUE, alpha=0) # , nlambda=50)
 
