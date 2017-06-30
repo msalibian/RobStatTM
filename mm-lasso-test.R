069e4f18f131f16263175ac7e92dc0e4a2cfa7ed
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-06-30 08:40:32 -0700

    checks

---
+++ mm-lasso-test.R
@@ -0,0 +1,41 @@
+
+# Auxiliary functions 
+# Tukey's rho
+rho <- function(u, cc=1.5477) {
+  w <- as.numeric( abs(u) <= cc )
+  v <- (u^2/(2)*(1-(u^2/(cc^2))+(u^4/(3*cc^4))))*w +(1-w)*(cc^2/6)
+  v <- v*6/cc^2
+  return(v)
+}
+
+# Integral of rho with respect to a N(0,1)
+rhoint <- function(e)
+  return(integrate(function(a, cc) rho(a, cc)*dnorm(a), cc=e, lower=-Inf, upper=+Inf)$value)
+
+# find constant for a desired BDP (delta)
+find.tuning.chi <- function(delta, low=.5, upp=10) {
+  return( uniroot( function(e) (rhoint(e)-delta), lower=low, upper=upp)$root )
+}
+
+# devtools::install_github('esmucler/mmlasso')
+library(mmlasso)
+
+n <- 50 # 500
+p <- 20 # 10
+set.seed(123)
+x <- matrix(rnorm(n*p), n, p)
+y <- as.vector( x %*% c(rep(2, 5), rep(0, p-5))) + rnorm(n, sd=.5)
+
+a <- sridge(x=x, y=y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0,
+            denormout=0, alone=1, ncores=4)
+
+# sanity check
+
+
+cc <- find.tuning.chi(a$delta)
+mean(rho(rnorm(1e6), cc=cc))
+re2 <- as.vector(y - x %*% a$coef[-1] - a$coef[1])
+sum(rho(re2/a$scale, cc=cc))/(n - a$edf)
+mean(rho(re2/a$scale, cc=cc))
+a$delta
+
--- mm-ridge-test.R
+++ mm-ridge-test.R
@@ -1,7 +1,7 @@
 
 # Check MM-ridge regression
 
-# install_github('gcohenfr/PENSE-package', ref='develop', auth_token='d49f60185d9de80d37dacef7f93b14df907e6af7')
+# devtools::install_github('gcohenfr/PENSE-package', ref='develop', auth_token='d49f60185d9de80d37dacef7f93b14df907e6af7')
 library(pense)
 
 # compare S-ridge as computed with pense
@@ -12,21 +12,53 @@ library(mmlasso)
 
 library(glmnet)
 
-n <- 500
-p <- 10
+n <- 50 # 500
+p <- 20 # 10
 set.seed(123)
 x <- matrix(rnorm(n*p), n, p)
 y <- as.vector( x %*% c(rep(2, 5), rep(0, p-5))) + rnorm(n, sd=.5)
 a <- sridge(x=x, y=y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0,
-            denormout=0, alone=1, ncores=1)
+            denormout=0, alone=1, ncores=4)
 
-b0 <- pense(X=x, y=y, alpha=0, standardize=TRUE, lambda=seq(1e-7, 5, length=20)) #,
+b0 <- pense(X=x, y=y, alpha=0, standardize=TRUE, lambda=1e-9) #,
 #           control=pense.control(mscale.delta = 0.49))
 
-b1 <- pense(X=x, y=y, alpha=0, lambda=seq(0, 5, length=20), standardize=TRUE,
-           control=pense.control(mscale.delta = 0.49))
+d <- elnet(X=x, y=y, alpha=0, lambda=1e-9, addLeading1s=TRUE)
+
+a$coef
+as.vector(b0$coef[,1])
+as.vector(d$coef[,1])
+c(a$scale, b0$scale)
+
+re <- as.vector(y - x %*% (b0$coef[,1])[-1] - b0$coef[1,1])
+mean(rho(re/b0$scale))
+
+cc <- find.tuning.chi(a$delta)
+mean(rho(rnorm(1e6), cc=cc))
+re2 <- as.vector(y - x %*% a$coef[-1] - a$coef[1])
+sum(rho(re2/a$scale, cc=cc))/(n - a$edf)
+mean(rho(re2/a$scale, cc=cc))
+a$delta
+
+# b1 <- pense(X=x, y=y, alpha=0, lambda=seq(0, 5, length=20), standardize=TRUE,
+#            control=pense.control(mscale.delta = 0.49))
+# 
+# 
+# d <- cv.glmnet(x=x, y=y, lambda=seq(0, 5, length=20), family='gaussian',
+#                intercept=TRUE, alpha=0) # , nlambda=50)
+
+rho <- function(u, cc=1.5477) {
+  w <- as.numeric( abs(u) <= cc )
+  v <- (u^2/(2)*(1-(u^2/(cc^2))+(u^4/(3*cc^4))))*w +(1-w)*(cc^2/6)
+  v <- v*6/cc^2
+  return(v)
+}
 
 
-d <- cv.glmnet(x=x, y=y, lambda=seq(0, 5, length=20), family='gaussian',
-               intercept=TRUE, alpha=0) # , nlambda=50)
+rhoint <- function(e)
+  return(integrate(function(a, cc) rho(a, cc)*dnorm(a), cc=e, lower=-Inf, upper=+Inf)$value)
 
+
+find.tuning.chi <- function(delta, low=.5, upp=10) {
+  return( uniroot( function(e) (rhoint(e)-delta), lower=low, upper=upp)$root )
+}
