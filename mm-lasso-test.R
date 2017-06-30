0363ab28056ee44fc91b9993e93dfb10905a4871
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-06-30 08:52:39 -0700

    sanity check for S-ridge

--- mm-lasso-test.R
+++ mm-lasso-test.R
@@ -8,15 +8,6 @@ rho <- function(u, cc=1.5477) {
   return(v)
 }
 
-# Integral of rho with respect to a N(0,1)
-rhoint <- function(e)
-  return(integrate(function(a, cc) rho(a, cc)*dnorm(a), cc=e, lower=-Inf, upper=+Inf)$value)
-
-# find constant for a desired BDP (delta)
-find.tuning.chi <- function(delta, low=.5, upp=10) {
-  return( uniroot( function(e) (rhoint(e)-delta), lower=low, upper=upp)$root )
-}
-
 # devtools::install_github('esmucler/mmlasso')
 library(mmlasso)
 
@@ -32,12 +23,10 @@ a <- sridge(x=x, y=y, cualcv.S=5, numlam.S=30, niter.S=50, normin=0,
             denormout=0, alone=1, ncores=4)
 
 # sanity check
-# find tuning constant for adjusted delta
-cc <- find.tuning.chi(a$delta)
 # get residuals
 re2 <- as.vector(y - x %*% a$coef[-1] - a$coef[1])
 # check the M-scale equation
-mean(rho(re2/a$scale, cc=cc))
+mean(rho(re2/a$scale))
 a$delta
 sum(rho(re2/a$scale, cc=cc))/(n - a$edf)
 
