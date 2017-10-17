6f5ecfd188cc4a0ca4c60c3df4546590e71ade98
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-10-17 06:57:04 -0700

    mm-lasso examples

--- R/MM-LASSO-example.R
+++ R/MM-LASSO-example.R
@@ -33,17 +33,17 @@ cbind(as.vector(a$coef.MMLasso), coef(b2))
 
 # Again with different values of n & p
 
-n <- 100
-p <- 200
+n <- 50
+p <- 100
 set.seed(123)
 x <- matrix(rnorm(n*p), n, p)
 y <- as.vector( x %*% c(rep(7, 25), rep(0, p-25))) + rnorm(n, sd=1.5)
 
 a <- mmlasso(x=x, y=y, ncores=2)
-b1 <- pense(X=x, y=y, alpha=0, nlambda=30, ncores=2, 
-            init_options = initest_options(maxit=1, maxit_pense_refinement=1))
+b1 <- pense(X=x, y=y, alpha=0, nlambda=30, ncores=2) 
+            #init_options = initest_options(maxit=1, maxit_pense_refinement=1))
 b2 <- pensem(b1, alpha=1, nlambda=30, ncores=2)
-rbind(as.vector(a$coef.MMLasso), coef(b2))
+cbind(as.vector(a$coef.MMLasso), coef(b2))
 
 
 
