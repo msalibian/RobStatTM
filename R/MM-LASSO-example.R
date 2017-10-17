d0469389392f640eed1e2387ba4f5a5ca5085fff
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-10-17 07:16:41 -0700

    better signals in the examples?

--- R/MM-LASSO-example.R
+++ R/MM-LASSO-example.R
@@ -35,9 +35,9 @@ cbind(as.vector(a$coef.MMLasso), coef(b2))
 
 n <- 50
 p <- 100
-set.seed(123456)
+set.seed(123)
 x <- matrix(rnorm(n*p), n, p)
-y <- as.vector( x %*% c(rep(7, 25), rep(0, p-25))) + rnorm(n, sd=1.5)
+y <- as.vector( x %*% c(rep(7, 25), rep(0, p-25))) + rnorm(n, sd=.5)
 
 a <- mmlasso(x=x, y=y, ncores=2)
 b1 <- pense(X=x, y=y, alpha=0, nlambda=30, ncores=2) #init_options = initest_options(maxit=1, maxit_pense_refinement=1))
