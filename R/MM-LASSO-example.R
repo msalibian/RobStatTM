e43e5d44967c33db34149d645f9a1410add7394f
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-10-17 06:47:14 -0700

    Examples for MM-LASSO

---
+++ R/MM-LASSO-example.R
@@ -0,0 +1,50 @@
+
+
+# ## Only run once
+# # Install pense package
+# library(devtools)
+# install_github("dakep/pense-rpkg", ref = "develop", force=TRUE)
+# 
+# # Install pyinit package
+# library(devtools)
+# install_github("dakep/pyinit", ref = "master", force=TRUE)
+
+
+library(pense)
+
+n <- 80
+p <- 50
+set.seed(123)
+x <- matrix(rnorm(n*p), n, p)
+y <- as.vector( x %*% c(rep(7, 5), rep(0, p-5))) + rnorm(n, sd=.5)
+
+
+# Compute the S-ridge (EN with alpha = 0)
+b0 <- pense(X=x, y=y, alpha=0, ncores=4, nlambda=30)
+
+# Compute MM-LASSO starting from the S-ridge (and the assoc. scale)
+b2 <- pensem(b0, alpha=1, nlambda=30, ncores=4, nlambda=30)
+
+# Compare with Ezequiel's MM-LASSO
+library(mmlasso)
+a <- mmlasso(x=x, y=y, ncores=4)
+
+cbind(as.vector(a$coef.MMLasso), coef(b2))
+
+# Again with different values of n & p
+
+n <- 100
+p <- 200
+set.seed(123)
+x <- matrix(rnorm(n*p), n, p)
+y <- as.vector( x %*% c(rep(7, 25), rep(0, p-25))) + rnorm(n, sd=1.5)
+
+a <- mmlasso(x=x, y=y, ncores=2)
+b1 <- pense(X=x, y=y, alpha=0, nlambda=30, ncores=2, 
+            init_options = initest_options(maxit=1, maxit_pense_refinement=1))
+b2 <- pensem(b1, alpha=1, nlambda=30, ncores=2)
+rbind(as.vector(a$coef.MMLasso), coef(b2))
+
+
+
+
