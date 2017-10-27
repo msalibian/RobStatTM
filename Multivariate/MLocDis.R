65ba8d664fdab56c30364c36978c33962313f364
Matias Salibian Barrera <matias@stat.ubc.ca>
2017-10-27 14:03:41 -0700

    Tidy up, include help

--- Multivariate/MLocDis.R
+++ Multivariate/MLocDis.R
@@ -1,12 +1,33 @@
-#source("MScale.R")
+#' Robust multivariate location and scatter M-estimators
+#'
+#' This function computes M-estimators for multivariate location and scatter.
+#'
+#' This function computes M-estimators for multivariate location and scatter.
+#'
+#M-estimators of location and dispersion
+#psi= "Bis" (quare) (default) of "Hub" (er)
+#eff=desired asymptotic efficiency. Options: 0.9 (default), 0.85 or 0.95
+#Output: mu=location estimator, std.mu= estimated standard deviation of mu,
+# disper= M-dispersion estimator
 
-MLocDis<- function(x,psi="Bis",eff=0.9, maxit=50, tol=1.e-4) {
-  #M-estimators of location and dispersion
-  #psi= "Bis" (quare) (default) of "Hub" (er)
-  #eff=desired asymptotic efficiency. Options: 0.9 (default), 0.85 or 0.95
-  #Output: mu=location estimator, std.mu= estimated standard deviation of mu, 
-  # disper= M-dispersion estimator
-  ########
+#' @param x a data matrix with observations in rows.
+#' @param psi a string indicating which score function to use. Valid options are "Bis" for
+#' bi-square and "Hub" for a Huber-type.
+#' @param eff desired asymptotic efficiency. Valid options are 0.9 (default), 0.85 and 0.95.
+#' @param maxit maximum number of iterations allowed.
+#' @param tol tolerance to decide convergence of the iterative algorithm.
+#'
+#' @return A list with the following components:
+#' \item{mu}{The location estimator}
+#' \item{std.mu}{Estimated standard deviation of the location estimator \code{mu}}
+#' \item{disper}{M-dispersion estimator}
+#'
+#' @author Ricardo Maronna, \email{rmaronna@retina.ar}
+#'
+#' @references \url{http://thebook}
+#'
+#' @export
+MLocDis<- function(x, psi="Bis", eff=0.9, maxit=50, tol=1.e-4) {
   if (psi=="Bis") {kpsi=1
   } else  if (psi=="Hub") {kpsi=2
   } else {print(c(psi, " No such psi")); kpsi=0
@@ -15,7 +36,7 @@ MLocDis<- function(x,psi="Bis",eff=0.9, maxit=50, tol=1.e-4) {
   kHub=c(0.732, 0.981, 1.34)
   kk=rbind(kBis, kHub)
   efis=c(0.85, 0.90, 0.95)
-  if (is.element(eff, efis)) {keff=match(eff,efis); 
+  if (is.element(eff, efis)) {keff=match(eff,efis);
   } else {print(c(eff, " No such eff")); keff=0}
 if (kpsi>0 & keff>0) {
   ktun=kk[kpsi, keff]
