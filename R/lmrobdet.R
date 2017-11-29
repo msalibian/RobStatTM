8ad2eeaeb2d0f733fc40d99dea9166fccb417e8a
Matias Salibian Barrera <matias@stat.ubc.ca>
2017-11-29 12:08:53 -0800

    removed seed, re-ordered list of arguments

--- R/lmrobdet.R
+++ R/lmrobdet.R
@@ -329,8 +329,8 @@ lmrobdetMM <- function(formula, data, subset, weights, na.action,
 
 #' Tuning parameters for lmrobdetMM and lmrobdetDCML
 #'
-#' This function sets tuning parameters for the MM estimator implemented in \code{lmrobdetMM} and  
-#' the Distance Constrained Maximum Likelihood regression estimators 
+#' This function sets tuning parameters for the MM estimator implemented in \code{lmrobdetMM} and
+#' the Distance Constrained Maximum Likelihood regression estimators
 #' computed by \code{lmrobdetDCML}.
 #'
 #' There are 2 sets of tuning parameters: those related to the MM-estimator,
@@ -339,14 +339,14 @@ lmrobdetMM <- function(formula, data, subset, weights, na.action,
 #' @rdname lmrobdet.control
 #' @param tuning.chi tuning constant for the function used to compute the M-scale
 #' for the S-estimator. If missing, it is computed inside \code{lmrobdet.control} to match
-#' the value of \code{bb} below according to the family of rho functions specified in \code{family}. 
+#' the value of \code{bb} below according to the family of rho functions specified in \code{family}.
 #' @param bb tuning constant (between 0 and 1/2) for the M-scale used to compute the initial S-estimator. It
 #' determines the robusness (breakdown point) of the resulting MM-estimator, which is
 #' \code{bb}. Defaults to 0.5.
 #' @param tuning.psi tuning parameters for the regression M-estimator computed with a rho function
 #' as specified with argument \code{family}. If missing, it is computed inside \code{lmrobdet.control} to match
 #' the value of \code{efficiency} according to the family of rho functions specified in \code{family}.
-#' @param efficiency desired asymptotic efficiency of the final regression M-estimator. Defaults to 0.85. 
+#' @param efficiency desired asymptotic efficiency of the final regression M-estimator. Defaults to 0.85.
 #' @param max.it maximum number of IRWLS iterations for the MM-estimator
 #' @param refine.tol relative covergence tolerance for the S-estimator
 #' @param rel.tol relative covergence tolerance for the IRWLS iterations for the MM-estimator
@@ -357,7 +357,7 @@ lmrobdetMM <- function(formula, data, subset, weights, na.action,
 #' that performs M-iterations (lmrob..M..fit), so set here.
 #' @param compute.rd logical value indicating whether robust leverage distances need to be computed.
 #' @param family string specifying the name of the family of loss function to be used (current valid
-#' options are "bisquare", "optimal" and "modified.optimal"). 
+#' options are "bisquare", "optimal" and "modified.optimal").
 #' @param corr.b logical value indicating whether a finite-sample correction should be applied
 #' to the M-scale parameter \code{bb}
 #' @param split.type determines how categorical and continuous variables are split. See
@@ -371,14 +371,13 @@ lmrobdetMM <- function(formula, data, subset, weights, na.action,
 #' \code{"threshold"}, all observations with scaled residuals larger than \code{C.res} will
 #' be removed, if \code{"proportion"}, observations with the largest \code{prop} residuals will
 #' be removed. See \code{\link{pyinit}}.
-#' @param resid_keep_thresh See parameter \code{clean.method} above. See \code{\link{pyinit}}.
-#' @param resid_keep_prop See parameter \code{clean.method} above. See \code{\link{pyinit}}.
+#' @param resid_keep_thresh See parameter \code{resid_keep_method} above. See \code{\link{pyinit}}.
+#' @param resid_keep_prop See parameter \code{resid_keep_method} above. See \code{\link{pyinit}}.
 #' @param py_maxit Maximum number of iterations. See \code{\link{pyinit}}.
 #' @param py_eps Relative tolerance for convergence.  See \code{\link{pyinit}}.
 #' @param mscale_maxit Maximum number of iterations for the M-scale algorithm. See \code{\link{pyinit}}.
 #' @param mscale_tol Convergence tolerance for the M-scale algorithm. See \code{\link{pyinit}}.
 #' @param mscale_rho_fun String indicating the loss function used for the M-scale. See \code{\link{pyinit}}.
-#' @param seed \code{NULL}
 #'
 #' @return A list with the necessary tuning parameters.
 #'
@@ -391,37 +390,36 @@ lmrobdetMM <- function(formula, data, subset, weights, na.action,
 #' m2 <- lmrobdet(Y ~ ., data=coleman, control=lmrobdet.control(refine.PY=50))
 #'
 #' @export
-lmrobdet.control <- function(seed = NULL,
-                             bb = 0.5, # 50% Breakdown point
+lmrobdet.control <- function(bb = 0.5, # 50% Breakdown point
                              efficiency = 0.85, # 85% efficiency
                              family = 'bisquare',
                              tuning.psi, # = findTuningConstFromEfficiency(efficiency, family),
                              tuning.chi, # = findTuningConstFromBDP(bb, family),
-                             max.it = 100, refine.tol = 1e-7, rel.tol = 1e-7,
-                             refine.PY = 10, # no. of steps to refine PY candidates
-                             solve.tol = 1e-7, trace.lev = 0, mts = 1000,
                              compute.rd = FALSE, # psi = 'bisquare',
                              corr.b = TRUE, # for MMPY and SMPY
                              split.type = "f", # help(splitFrame, package='robustbase')
                              initial='S', #'S' or 'MS'
+                             max.it = 100, refine.tol = 1e-7, rel.tol = 1e-7,
+                             refine.PY = 10, # no. of steps to refine PY candidates
+                             solve.tol = 1e-7, trace.lev = 0,
                              psc_keep = 0.5, resid_keep_method = 'threshold',
                              resid_keep_thresh = 2, resid_keep_prop = .2, py_maxit = 20, py_eps = 1e-5,
-                             mscale_maxit = 50, mscale_tol = 1e-06,
-                             mscale_rho_fun = 'bisquare')
+                             mscale_maxit = 50, mscale_tol = 1e-06, mscale_rho_fun = 'bisquare',
+                             mts = 1000)
 {
   if(family == 'bisquare') {
     if(missing(tuning.psi))
       tuning.psi <- findTuningConstFromEfficiencyBisquare(efficiency)
-    if(missing(tuning.chi)) 
+    if(missing(tuning.chi))
       tuning.chi <- findTuningConstFromBDPBisquare(bb)
   }
-  
+
   # does not run
   if(family != 'bisquare') {
     family <- match.arg(family, choices = FAMILY.NAMES)
     tuning.chi <- adjustTuningVectorForBreakdownPoint(do.call(family, args=list(e=efficiency)), breakdown.point = bb)
-  }  
-  
+  }
+
   return(list(seed = as.integer(seed), family=family, # psi=psi,
               tuning.chi=tuning.chi, bb=bb, tuning.psi=tuning.psi,
               max.it=max.it,
@@ -648,7 +646,7 @@ print.summary.lmrobdetMM <- function (x, digits = max(3, getOption("digits") - 3
 #' @export
 refine.sm <- function(x, y, initial.beta, initial.scale, k=50,
                       conv=1, b, cc, family, step='M') {
- 
+
 #refine.sm <- function(x, y, initial.beta, initial.scale, k=50,
 #                     conv=1, b, cc, step='M') {
 
@@ -660,8 +658,8 @@ refine.sm <- function(x, y, initial.beta, initial.scale, k=50,
   #}
   f.w <- function(u, family, cc)
     Mwgt(x = u, cc = cc, psi = family)
-  
-  
+
+
   n <- dim(x)[1]
   # p <- dim(x)[2]
 
@@ -1231,7 +1229,7 @@ rob.linear.test <- function(object1, object2)
   p <- length(object1$coeff)
   q <- length(object1$coeff) - length(object2$coeff)
   n <- length(object1$resid)
-  
+
   family1 <- object1$control$family
   family2 <- object2$control$family
   cc1 <- object1$control$tuning.psi
@@ -1239,7 +1237,7 @@ rob.linear.test <- function(object1, object2)
 
   same.name <- (family1 == family2)
   same.cc <- isTRUE(all.equal(cc1, cc2))
-  
+
   if(!(same.name && same.cc))
     stop("object1 and object2 do not have the same rho family")
 
