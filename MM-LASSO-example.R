1b0545ff656fc56ac2af1fc296042a848a18ef3f
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-11-28 21:36:02 -0800

    glass data example

--- MM-LASSO-example.R
+++ MM-LASSO-example.R
@@ -44,6 +44,28 @@ b1 <- pense(X=x, y=y, alpha=0, nlambda=30, ncores=2) #init_options = initest_opt
 b2 <- pensem(b1, alpha=1, nlambda=30, ncores=2)
 round(cbind(as.vector(a$coef.MMLasso), coef(b2)), 3)
 
+# install.packages('cellWise')
+# data(glass, package='cellWise')
+# data(glassVessels, package='locout')
 
+load('glassVessels.RData')
+x <- glass$data
+# table(glass$group)
+# matplot(t(x), col='gray', type='l', lty=1)
+y <- glass$chemical[,'PbO']
+x <- x[, 15:500]
+library(glmnet)
+set.seed(123)
+tmp <- cv.glmnet(y=y, x=x, nfolds=10, alpha=1)
+plot(tmp)
+as.vector( coef(tmp, s='lambda.1se') )
 
+library(mmlasso)
+a <- mmlasso(x=x, y=y, ncores=4)
+
+library(pense)
+# Compute the S-ridge (EN with alpha = 0)
+b0 <- pense(x=x, y=y, alpha=0, ncores=4, nlambda=30)
+# Compute MM-LASSO starting from the S-ridge (and the assoc. scale)
+b2 <- pensem(b0, alpha=1, nlambda=30, ncores=4)
 
