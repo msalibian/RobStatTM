a7c3f29ce98ae33b46a5d1323a509a2e60a292b9
Matias Salibian-Barrera <matias@stat.ubc.ca>
2017-12-04 16:40:38 -0800

    include robust R^2 if bisquare

--- R/lmrobdet.R
+++ R/lmrobdet.R
@@ -180,6 +180,8 @@ lmrobdetMM <- function(formula, data, subset, weights, na.action,
       z$scale <- mscale(u=z$resid, tol = control$mscale_tol, delta=control$bb*(1-p/length(z$resid)), tuning.chi=control$tuning.chi, family=control$family)
       # compute robust R^2
       #s2 <- sum(rho(z$resid/z$scale, cc=z$control$tuning.psi))
+      r.squared <- adj.r.squared <- 0
+      if(control$family == 'bisquare') {
       s2 <- sum(rho(z$resid/z$scale, family = control$family, cc=control$tuning.psi))
       if( p != attr(mt, "intercept") ) {
         df.int <- if (attr(mt, "intercept"))
@@ -199,11 +201,12 @@ lmrobdetMM <- function(formula, data, subset, weights, na.action,
           s02 <- sum(rho(y/z$scale, family = control$family, cc=control$tuning.psi))
         }
         # INVTR2(tmp2$r.squared, tmp2$control$tuning.psi)
-        r.squared <- NA #INVTR2( (s02 - s2)/s02, control$tuning.psi)
+        r.squared <- INVTR2( (s02 - s2)/(s02*(1-s2)), control$tuning.psi)
         # ((n-1)/(n-p))R^2 -((p-1)/(n-p))
-        adj.r.squared <- NA #((n-1)/(n-p))*r.squared -(p-1)/(n-p) # ( s02/(n-1) - s2/(n-z$rank) ) / (s02/(n-1)) # n-p? p.193
+        adj.r.squared <- ((n-1)/(n-p))*r.squared -(p-1)/(n-p) # ( s02/(n-1) - s2/(n-z$rank) ) / (s02/(n-1)) # n-p? p.193
       }
-      else r.squared <- adj.r.squared <- 0
+      }
+      # else r.squared <- adj.r.squared <- 0
       z$r.squared <- r.squared
       z$adj.r.squared <- adj.r.squared
       # DCML
