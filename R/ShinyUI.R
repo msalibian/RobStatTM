0a793c350ed1a0e434a3e977aa431625ae92cd59
Matias Salibian Barrera <matias@stat.ubc.ca>
2018-08-09 10:33:22 -0700

    openShinyUI -> ShinyUI

--- R/ShinyUI.R
+++ R/ShinyUI.R
@@ -4,13 +4,13 @@
 #'
 #' @author Gregory Brownson \email{gsb25@uw.edu}
 #' @references \url{http://thebook}
-#' 
+#'
 #' @export
-openShinyUI <- function() {
+ShinyUI <- function() {
   appDir <- system.file("shiny-ui", package = "RobStatTM")
   if (appDir == "") {
     stop("Could not find Shiny UI directory. Try re-installing `RobStatTM`.", call. = FALSE)
   }
-  
+
   shiny::runApp(appDir, display.mode = "normal")
-}  
+}
