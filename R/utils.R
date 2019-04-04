dd844da5befdf2c816735ed978958261b67d5052
Gregory <gregory.brownson@outlook.com>
2019-04-03 21:56:32 -0700

    Change from 'onAttach' to 'onLoad'

--- R/utils.R
+++ R/utils.R
@@ -1,17 +1,25 @@
 #' @import fit.models
 
-.onAttach <- function(libname, pkgname) {
-  fit.models::fmclass.add.class("lmfm", "lmrobM", warn = F)
-  fit.models::fmclass.add.class("lmfm", "lmrobdetMM", warn = F)
-  fit.models::fmclass.add.class("lmfm", "lmrobdetDCML", warn = F)
+.onLoad <- function(libname, pkgname) {
+  library.dynam("RobStatTM", package = pkgname, lib.loc = libname)
   
-  fit.models::fmclass.register(fmclass = "covfm",
-                   classes = c("covClassic", "covRob"),
-                   validation.function = NULL)
+  ##--------------- begin {fit.models} -----------------
+  requireNamespace("fit.models")
+  FM.add.class <- fit.models::fmclass.add.class
+  FM.register  <- fit.models::fmclass.register
+  
+  FM.add.class("lmfm", "lmrobM", warn = F)
+  FM.add.class("lmfm", "lmrobdetMM", warn = F)
+  FM.add.class("lmfm", "lmrobdetDCML", warn = F)
+  
+  FM.register(fmclass = "covfm",
+              classes = c("covClassic", "covRob"),
+              validation.function = NULL)
                                  
-  fit.models::fmclass.register(fmclass = "pcompfm",
-                   classes = c("prcomp", "prcompRob"),
-                   validation.function = NULL)
+  FM.register(fmclass = "pcompfm",
+              classes = c("prcomp", "prcompRob"),
+              validation.function = NULL)
+  ##--------------- end {fit.models} -------------------
 
   invisible()
 }
