4a60a1b2229d685f35e488c0bd7a37fbb0e1b6b2
Matias Salibian-Barrera <matias@stat.ubc.ca>
2019-04-04 10:20:47 -0700

    .onLoad() seems to require .onUnload()

--- R/utils.R
+++ R/utils.R
@@ -2,20 +2,20 @@
 
 .onLoad <- function(libname, pkgname) {
   library.dynam("RobStatTM", package = pkgname, lib.loc = libname)
-  
+
   ##--------------- begin {fit.models} -----------------
   requireNamespace("fit.models")
   FM.add.class <- fit.models::fmclass.add.class
   FM.register  <- fit.models::fmclass.register
-  
+
   FM.add.class("lmfm", "lmrobM", warn = F)
   FM.add.class("lmfm", "lmrobdetMM", warn = F)
   FM.add.class("lmfm", "lmrobdetDCML", warn = F)
-  
+
   FM.register(fmclass = "covfm",
               classes = c("covClassic", "covRob"),
               validation.function = NULL)
-                                 
+
   FM.register(fmclass = "pcompfm",
               classes = c("prcomp", "prcompRob"),
               validation.function = NULL)
@@ -23,3 +23,8 @@
 
   invisible()
 }
+
+.onUnload <- function (libpath) {
+  library.dynam.unload("RobStatTM", libpath)
+}
+
