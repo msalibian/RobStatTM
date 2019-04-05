8ae2872fe8d9e866f031d51400d144b9ce84bd55
Gregory <gregory.brownson@outlook.com>
2019-04-05 10:52:37 -0700

    Using add class method rather than register for covfm

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,6 @@
 #' @import fit.models
 
 .onLoad <- function(libname, pkgname) {
-  library.dynam("RobStatTM", package = pkgname, lib.loc = libname)
-  
   ##--------------- begin {fit.models} -----------------
   requireNamespace("fit.models")
   FM.add.class <- fit.models::fmclass.add.class
@@ -12,9 +10,12 @@
   FM.add.class("lmfm", "lmrobdetMM", warn = F)
   FM.add.class("lmfm", "lmrobdetDCML", warn = F)
   
-  FM.register(fmclass = "covfm",
-              classes = c("covClassic", "covRob"),
-              validation.function = NULL)
+  FM.add.class("covfm", "covClassic", warn = F)
+  FM.add.class("covfm", "covRob", warn = F)
+  
+  # FM.register(fmclass = "covfm",
+  #             classes = c("covClassic", "covRob"),
+  #             validation.function = NULL)
                                  
   FM.register(fmclass = "pcompfm",
               classes = c("prcomp", "prcompRob"),
