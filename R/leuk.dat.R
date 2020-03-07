ef81cf6c0e93c06bf0d5f4eb812238e51e773ea4
Matias Salibian-Barrera <matias@stat.ubc.ca>
2020-03-06 22:12:08 -0800

    add leuk.dat

---
+++ R/leuk.dat.R
@@ -0,0 +1,22 @@
+#' Leukemia Data
+#'
+#' A description of the data 
+#'
+#' Description: The following features are present
+#' wbc: wbc
+#' ag: ag
+#' y: y, the response.
+#'
+#' Format: Numeric, 33 rows and 3 columns.
+#'
+#' @docType data
+#'
+#' @usage data(leuk.dat)
+#'
+#' @format An object of class \code{"data.frame"}.
+#'
+#' @source Here is the source.
+#'
+#' @examples
+#' data(leuk.dat)
+"leuk.dat"
