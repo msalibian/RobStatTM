f7a2ec6916e79558736f790e02c044a28994d08a
Matias Salibian-Barrera <matias@stat.ubc.ca>
2020-07-27 15:27:47 -0700

    remove fit.models classes and definitions in RobStatTM

--- R/utils.R
+++ R/utils.R
@@ -1,22 +1,22 @@
-#' @import fit.models
+# #' @import fit.models
 
 .onLoad <- function(libname, pkgname) {
-  ##--------------- begin {fit.models} -----------------
-  requireNamespace("fit.models")
-  FM.add.class <- fit.models::fmclass.add.class
-  FM.register  <- fit.models::fmclass.register
-
-  FM.add.class("lmfm", "lmrobM", warn = F)
-  FM.add.class("lmfm", "lmrobdetMM", warn = F)
-  FM.add.class("lmfm", "lmrobdetDCML", warn = F)
-
-  FM.add.class("covfm", "covClassic", warn = F)
-  FM.add.class("covfm", "covRob", warn = F)
-  
-  FM.register(fmclass = "pcompfm",
-              classes = c("prcomp", "prcompRob"),
-              validation.function = NULL)
-  ##--------------- end {fit.models} -------------------
+  # ##--------------- begin {fit.models} -----------------
+  # requireNamespace("fit.models")
+  # FM.add.class <- fit.models::fmclass.add.class
+  # FM.register  <- fit.models::fmclass.register
+  # 
+  # FM.add.class("lmfm", "lmrobM", warn = F)
+  # FM.add.class("lmfm", "lmrobdetMM", warn = F)
+  # FM.add.class("lmfm", "lmrobdetDCML", warn = F)
+  # 
+  # FM.add.class("covfm", "covClassic", warn = F)
+  # FM.add.class("covfm", "covRob", warn = F)
+  # 
+  # FM.register(fmclass = "pcompfm",
+  #             classes = c("prcomp", "prcompRob"),
+  #             validation.function = NULL)
+  # ##--------------- end {fit.models} -------------------
 
   invisible()
 }
